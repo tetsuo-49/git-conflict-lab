962f33883715129ba85d438725dc74a6d9ddadc9
Ayoub Jabiri <ayoubjabiri.coding@gmail.com>
2026-01-20 17:04:26 +0100

    Added index file

--- app.js
+++ app.js
@@ -1,13 +1,12 @@
-  // Main Application
-  function calculateTotal(items) {
+// Main Application
+function calculateTotal(items) {
     let total = 0;
     for (let item of items) {
-      total += item.price;
+        total += item.price;
     }
     return total;
-  }
+}
 
-  function displayWelcome() {
+function displayWelcome() {
     console.log("Welcome to the Store!");
-  }
-
+}
